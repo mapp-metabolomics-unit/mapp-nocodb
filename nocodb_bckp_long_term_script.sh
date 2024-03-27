c4f97402f69d9a238d0bebd8220f4ef739d98c9d
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-03-27 14:36:54 +0100

    up

--- nocodb_bckp_long_term_script.sh
+++ nocodb_bckp_long_term_script.sh
@@ -2,8 +2,8 @@
 
 # Variables
 DATE=$(date +"%Y%m%d%H%M%S")
-BACKUP_DIR_LOCAL="/media/backup/nocodb_bckp/long_term_bckp/"
-BACKUP_DIR_DISTANT="/media/share/dbgi/nocodb_bckp/long_term_bckp/"
+BACKUP_DIR_LOCAL="/media/backup/nocodb_bckp/long_term_bckp"
+BACKUP_DIR_DISTANT="/media/share/dbgi/nocodb_bckp/long_term_bckp"
 LOG_FILE="/media/backup/nocodb_bckp/long_term_bckp/bckp.log"
 RETAIN_BACKUPS=2
 
@@ -13,8 +13,8 @@ exec &>> "$LOG_FILE"
 # Enable immediate exit on error
 set -e
 
-mkdir "${BACKUP_DIR_LOCAL}/${DATE}"
-mkdir "${BACKUP_DIR_DISTANT}/${DATE}"
+mkdir -p "${BACKUP_DIR_LOCAL}/${DATE}"
+mkdir -p "${BACKUP_DIR_DISTANT}/${DATE}"
 
 # Perform backup
 tar -czvf "${BACKUP_DIR_LOCAL}/${DATE}/backup.tar.gz" -C "$POSTGRES_DIR" .
