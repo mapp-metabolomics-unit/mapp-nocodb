9f89fe98e69ce8f18d017ca737a9652be9491e7b
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-03-27 13:59:56 +0100

    test

--- nocodb_bckp_short_term_script.sh
+++ nocodb_bckp_short_term_script.sh
@@ -18,8 +18,8 @@ mkdir $BACKUP_DIR_LOCAL
 mkdir $BACKUP_DIR_DISTANT
 
 # Perform backup
-tar -czvf "${BACKUP_DIR_LOCAL}/backup.tar.gz" -C $POSTGRES_DIR
-tar -czvf "${BACKUP_DIR_DISTANT}/backup.tar.gz" -C $POSTGRES_DIR
+tar -czvf "${BACKUP_DIR_LOCAL}/backup.tar.gz" -C "$POSTGRES_DIR" .
+tar -czvf "${BACKUP_DIR_DISTANT}/backup.tar.gz" -C "$POSTGRES_DIR" .
 
 # Check if backup was successful
 if [ $? -eq 0 ]; then
@@ -34,7 +34,7 @@ fi
 cleanup_backups() {
     local backup_dir="$1"
     if [ -n "$(ls -A "$backup_dir")" ]; then
-        ls -dt "$backup_dir"/* | tail -n +"$((RETAIN_BACKUPS+2))" | xargs rm -rf
+        ls -dt "$backup_dir"/* | tail -n +"$((RETAIN_BACKUPS+1))" | xargs rm -rf
     fi
 }
 
