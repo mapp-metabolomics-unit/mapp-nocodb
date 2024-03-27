dd7532f936997ea6ad850c6aa9a64fc3139760a9
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-03-27 14:44:14 +0100

    up

--- nocodb_bckp_long_term_script.sh
+++ nocodb_bckp_long_term_script.sh
@@ -2,8 +2,8 @@
 
 # Variables
 DATE=$(date +"%Y%m%d%H%M%S")
-BACKUP_DIR_LOCAL="/media/backup/nocodb_bckp/long_term_bckp/"
-BACKUP_DIR_DISTANT="/media/share/dbgi/nocodb_bckp/long_term_bckp/"
+BACKUP_DIR_LOCAL="/media/backup/nocodb_bckp/long_term_bckp"
+BACKUP_DIR_DISTANT="/media/share/dbgi/nocodb_bckp/long_term_bckp"
 LOG_FILE="/media/backup/nocodb_bckp/long_term_bckp/bckp.log"
 RETAIN_BACKUPS=2
 
@@ -18,7 +18,7 @@ mkdir -p "${BACKUP_DIR_DISTANT}/${DATE}"
 
 # Perform backup
 tar -czvf "${BACKUP_DIR_LOCAL}/${DATE}/backup.tar.gz" -C "$POSTGRES_DIR" .
-tar -czvf "${BACKUP_DIR_DISTANT}/${DATE}/backup.tar.gz" -C "$POSTGRES_DIR" .
+#tar -czvf "${BACKUP_DIR_DISTANT}/${DATE}/backup.tar.gz" -C "$POSTGRES_DIR" .
 
 # Check if backup was successful
 if [ $? -eq 0 ]; then
