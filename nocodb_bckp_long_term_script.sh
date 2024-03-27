82f16dd605d2851e33435494eef485cc43c981f5
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-03-27 14:29:17 +0100

    up

--- nocodb_bckp_long_term_script.sh
+++ nocodb_bckp_long_term_script.sh
@@ -2,8 +2,8 @@
 
 # Variables
 DATE=$(date +"%Y%m%d%H%M%S")
-BACKUP_DIR_LOCAL="/media/backup/nocodb_bckp/long_term_bckp/${DATE}"
-BACKUP_DIR_DISTANT="/media/share/dbgi/nocodb_bckp/long_term_bckp/${DATE}"
+BACKUP_DIR_LOCAL="/media/backup/nocodb_bckp/long_term_bckp"
+BACKUP_DIR_DISTANT="/media/share/dbgi/nocodb_bckp/long_term_bckp"
 LOG_FILE="/media/backup/nocodb_bckp/long_term_bckp/bckp.log"
 
 # Redirect all output to the log file
@@ -12,17 +12,17 @@ exec &>> "$LOG_FILE"
 # Enable immediate exit on error
 set -e
 
-mkdir $BACKUP_DIR_LOCAL
-mkdir $BACKUP_DIR_DISTANT
+mkdir "${BACKUP_DIR_LOCAL}/${DATE}"
+mkdir "${BACKUP_DIR_DISTANT}/${DATE}"
 
 # Perform backup
-tar -czvf "${BACKUP_DIR_LOCAL}/backup.tar.gz" -C "$POSTGRES_DIR" .
-tar -czvf "${BACKUP_DIR_DISTANT}/backup.tar.gz" -C "$POSTGRES_DIR" .
+tar -czvf "${BACKUP_DIR_LOCAL}/${DATE}/backup.tar.gz" -C "$POSTGRES_DIR" .
+tar -czvf "${BACKUP_DIR_DISTANT}/${DATE}/backup.tar.gz" -C "$POSTGRES_DIR" .
 
 # Check if backup was successful
 if [ $? -eq 0 ]; then
-    echo "Backup completed successfully: ${BACKUP_DIR_LOCAL}/backup.tar.gz"
-    echo "Backup completed successfully: ${BACKUP_DIR_DISTANT}/backup.tar.gz"
+    echo "Backup completed successfully: ${BACKUP_DIR_LOCAL}/${DATE}/backup.tar.gz"
+    echo "Backup completed successfully: ${BACKUP_DIR_DISTANT}/${DATE}/backup.tar.gz"
 else
     echo "Backup failed"
     exit 1
